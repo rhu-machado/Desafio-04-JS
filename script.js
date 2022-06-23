e2c4ce10fe9de96af236195c290c258b5242a723
rhu-machado <rhu.machado16@gmail.com>
2022-06-22 23:34:45 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+function mensagem(){
+    console.log("Olá, jovem ninja")
+}
+
+mensagem()
+
+let nome = "Rhuan"
+
+function fala(name){
+    console.log(`Então, você é o famoso ${name}?!`)
+}
+
+fala(nome)
+
+let idade = 22
+let estilo = 'MPB'
+
+function sobre(name,age,music){
+    console.log(`Nome: ${name}, Idade: ${age}, Estilo musical preferido: ${music}`)
+}
+
+sobre(nome, idade, estilo)
+
+let filme = 'Guardiões da Galaxia'
+let musica = 'Felicidade'
+
+function gosto(f,m){
+    console.log(f,m)
+}
+gosto(filme, musica)
+
